4d782cc9e79b0da26a51fa741548865be173adb1
kedar-vaidya <kedarvaidya0@gmail.com>
2011-06-04 17:48:50 +0530

    Added user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,11 +26,20 @@ module SessionsHelper
     user == current_user
   end
   
+  def authenticate
+    deny_access unless signed_in?
+  end
+
   def deny_access
     store_location
-    flash[:notice] = "Please sign in to access this page."
-    redirect_to signin_path
+    redirect_to signin_path, :notice => "Please sign in to access this page."
   end
+  
+  #def deny_access
+   # store_location
+   # flash[:notice] = "Please sign in to access this page."
+   # redirect_to signin_path
+  #end
   
   def redirect_back_or(default)
     redirect_to(session[:return_to] || default)
